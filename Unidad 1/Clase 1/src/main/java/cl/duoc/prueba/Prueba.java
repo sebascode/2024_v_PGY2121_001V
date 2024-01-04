56a4584719fb83491e1bec3deaccb3a7b1ebe9c9
Sebas Contreras <swf.sebas@gmail.com>
2024-01-03 22:06:55 -0300

    Add files via upload

---
+++ Unidad 1/Clase 1/src/main/java/cl/duoc/prueba/Prueba.java
@@ -0,0 +1,20 @@
+package cl.duoc.prueba;
+
+
+public class Prueba {
+
+    public static void main(String[] args) {
+        // dos numeros que se sumen:
+        // dos variables numericas
+        // dos numeros almacenados en esas variables
+        // sumar esos dos numeros retornando el resultado.
+        // imprimir en pantalla el resultado.
+        
+        int primerNum = 5;
+        int segundoNum = 10;
+        
+        int resultado = primerNum + segundoNum;
+        
+        System.out.println("El resultado es: "+resultado);
+    }
+}
