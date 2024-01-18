0452813c15620373866c10203d1a3ea87d0ec35e
Sebas Contreras <swf.sebas@gmail.com>
2024-01-17 21:14:26 -0300

    Update IPersonaService.java

--- Unidad 2/Clase 11/MiServicio/src/main/java/cl/duoc/miservicio/service/IPersonaService.java
+++ Unidad 2/Clase 11/MiServicio/src/main/java/cl/duoc/miservicio/service/IPersonaService.java
@@ -3,6 +3,10 @@ package cl.duoc.miservicio.service;
 import cl.duoc.miservicio.models.Persona;
 
 public interface IPersonaService {
+    public static final int IVA = 19;
+    public static final int descuentoCarga = 3;
+    public static final int descuentoPasajeros = 7;
+    
     public void Guardar(Persona p);
     public void Borrar(int i);
     public void ImprimirListado();
